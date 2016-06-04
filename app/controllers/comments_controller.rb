4255247574c6f028d057bbd50cf6b88a80db4e49
Shari Jablon <Shari Jablon>
2016-06-04 10:48:04 -0400

    Lesson 5 Assignment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -24,12 +24,14 @@ class CommentsController < ApplicationController
   # POST /comments
   # POST /comments.json
   def create
+    @post = Post.find(params[:post_id])
     @comment = Comment.new(comment_params)
 
     respond_to do |format|
       if @comment.save
         format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
         format.json { render :show, status: :created, location: @comment }
+        format.js  #create.js.erb
       else
         format.html { render :new }
         format.json { render json: @comment.errors, status: :unprocessable_entity }
